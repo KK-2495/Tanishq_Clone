ed1a29e86796010e2f287f1720ed82f9f566c661
KK-2495 <krishnankalimuthunadar@hotmail.com>
2023-04-22 02:36:22 +0530

    Completed

---
+++ userData.js
@@ -0,0 +1,76 @@
+// *****Register**//
+function register(event) {
+  event.preventDefault();
+  var phNum = document.getElementById("userPhNum").value;
+  // console.log(phNum);
+  var name = document.getElementById("userName").value;
+  var password = document.getElementById("userPassword").value;
+  var confirmPassword = document.getElementById("userConfirmPassword").value;
+
+  if (phNum && name && password && confirmPassword) {
+    if (password.length >= 8 && confirmPassword.length >= 8) {
+      if (password == confirmPassword) {
+        var userInfo = {
+          tanishqUserPhNum: phNum,
+          tanishqUserName: name,
+          tanishqUserPass: password,
+          tanishqConfirmPass: confirmPassword,
+        };
+        // console.log(userInfo);
+        var multiUsers = JSON.parse(localStorage.getItem("tanishqUsers")) || [];
+        // console.log(multiUsers);
+        var flagForEmail = false;
+
+        for (var i = 0; i < multiUsers.length; i++) {
+            // console.log(multiUsers[i].tanishqUserPhNum);
+          if (multiUsers[i].tanishqUserPhNum == phNum) {
+            flagForEmail = true;
+          }
+        }
+        if (!flagForEmail) {
+          multiUsers.push(userInfo);
+
+          localStorage.setItem("tanishqUsers", JSON.stringify(multiUsers));
+          alert("Registered Successful.");
+          window.location.href = `./Login.html`;
+        } else {
+          alert("You're Already Registered with this Email");
+        }
+      } else {
+        alert("Passwords doesn't Match.");
+      }
+    } else {
+      alert("Passwords should be more than Eight");
+    }
+  } else {
+    alert("All fields are mandatory.");
+  }
+}
+
+// **********Login funcitn*****//
+ 
+function login(event) {
+    event.preventDefault();
+    var phNum = document.getElementById("userPhNum").value;
+
+    var tanishqLogin = JSON.parse(localStorage.getItem("tanishqUsers"));
+    // console.log(tanishqLogin);
+
+    var flagForPhNum = false;
+    var currentUser;
+    for(var i=0; i<tanishqLogin.length; i++){
+        // console.log(tanishqLogin[i].tanishqUserName);
+        if(tanishqLogin[i].tanishqUserPhNum == phNum){
+            flagForPhNum = true;
+            currentUser = tanishqLogin[i];
+            // console.log(currentUser);
+        }
+    }if(flagForPhNum == true){
+        alert("Logged in Succesfully.")
+        localStorage.setItem("tanishqActiveUser", JSON.stringify(currentUser));
+        window.location.href = `./HomePage.html`;
+    }else{
+        alert("Please Register to Login")
+    }
+    
+}
